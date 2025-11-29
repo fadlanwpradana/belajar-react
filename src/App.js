24212093168e1eec6025692c450a454e3273e09b
fadlanwpradana <fadlanwpradana@gmail.com>
2025-11-29 22:23:29 +0700

    fix: change description text

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. pepek kau
-        </p>
+        <p>test aplikasi fadlan widya pradana</p>
         <a
           className="App-link"
           href="https://reactjs.org"

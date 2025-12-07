7823f9add8af085aeaf54aa3ee62c2382be0a9ac
fadlanwpradana <fadlanwpradana@gmail.com>
2025-12-07 23:29:03 +0700

    change api base url

--- src/service/api/axios.js
+++ src/service/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://grohosbouu-kingsman-v1.vercel.app/api/auth",
   timeout: 8000,
   headers: { "Content-Type": "application/json" },
 });
